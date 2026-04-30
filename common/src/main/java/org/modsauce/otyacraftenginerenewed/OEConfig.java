0f219ff5625dbe28470582c2e226c4be1b93034c
gly091020 <gulangyu1020@qq.com>
2026-04-30 20:26:15 +0200

    Just a regex modification for images. (#16)
    Modified the image regex.
    
    Co-authored-by: Shadowbee27 <shadowbee.contact@proton.me>
    Reviewed-on: https://codeberg.org/Mod-Sauce/OtyacraftEngineRenewed/pulls/16
    Reviewed-by: Fr4gm3nt3d_sh <fr4gm3nt3d_sh@noreply.codeberg.org>

--- common/src/main/java/org/modsauce/otyacraftenginerenewed/OEConfig.java
+++ common/src/main/java/org/modsauce/otyacraftenginerenewed/OEConfig.java
@@ -65,7 +65,7 @@ public class OEConfig extends PartitioningSerializer.GlobalData {
     public static class URLTextureConfig {
       private int maxLoaderCount = 3;
 
-      private String urlRegex = "https?://(i.imgur.com|imgur.com|.*music.126.*)/.*";
+      private String urlRegex = ".*";
 
       public int getMaxLoaderCount() {
         return Math.max(1, maxLoaderCount);
@@ -74,8 +74,9 @@ public class OEConfig extends PartitioningSerializer.GlobalData {
       public String getUrlRegex() {
           if(urlRegex.equals("https://(i.imgur.com|imgur.com)/.*") ||
                   urlRegex.equals("https?://(i.imgur.com|imgur.com|p2.music.126.net)/.*") ||
-                  urlRegex.equals("https?://(i.imgur.com|imgur.com|.*music.126.net)/.*"))
-              urlRegex = "https?://(i.imgur.com|imgur.com|.*music.126.*)/.*";
+                  urlRegex.equals("https?://(i.imgur.com|imgur.com|.*music.126.net)/.*") ||
+                  urlRegex.equals("https?://(i.imgur.com|imgur.com|.*music.126.*)/.*"))
+              urlRegex = ".*";
         return urlRegex;
       }
     }
