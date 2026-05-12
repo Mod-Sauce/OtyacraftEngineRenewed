e110901dc1dad58ce9e865802463cf986fdf8d34
Shadowbee <shadowbee.contact@proton.me>
2026-05-12 21:13:19 +0200

    Fix:) Mixin error

--- fabric/src/main/java/dev/felnull/otyacraftengine/explatform/fabric/OERegisterExpectPlatformImpl.java
+++ fabric/src/main/java/dev/felnull/otyacraftengine/explatform/fabric/OERegisterExpectPlatformImpl.java
@@ -2,9 +2,8 @@ package dev.felnull.otyacraftengine.explatform.fabric;
 
 import dev.architectury.registry.registries.RegistrySupplier;
 import dev.felnull.otyacraftengine.blockentity.BlockEntityCreateSupplier;
-import dev.felnull.otyacraftengine.fabric.mixin.PoiTypesInvoker;
+import java.util.function.Supplier;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.item.Tier;
@@ -14,20 +13,28 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Supplier;
-
 public class OERegisterExpectPlatformImpl {
-    public static <T extends BlockEntity> BlockEntityType<T> createBlockEntity(BlockEntityCreateSupplier<? extends T> supplier, Block... blocks) {
-        FabricBlockEntityTypeBuilder<T> builder = FabricBlockEntityTypeBuilder.create(supplier::create, blocks);
-        return builder.build();
-    }
 
-    public static void registerPoiTypeBlockStates(RegistrySupplier<PoiType> poiTypeRegistrySupplier) {
-        var rk = BuiltInRegistries.POINT_OF_INTEREST_TYPE.getResourceKey(poiTypeRegistrySupplier.get()).orElseThrow();
-        PoiTypesInvoker.invokeRegisterBlockStates(BuiltInRegistries.POINT_OF_INTEREST_TYPE.getHolderOrThrow(rk), poiTypeRegistrySupplier.get().matchingStates());
-    }
+  public static <T extends BlockEntity> BlockEntityType<
+    T
+  > createBlockEntity(
+    BlockEntityCreateSupplier<? extends T> supplier,
+    Block... blocks
+  ) {
+    FabricBlockEntityTypeBuilder<T> builder =
+      FabricBlockEntityTypeBuilder.create(supplier::create, blocks);
+    return builder.build();
+  }
 
-    public static Tier createTier(int level, int uses, float speed, float attackDamageBonus, int enchantmentValue, @NotNull TagKey<Block> tag, @NotNull Supplier<Ingredient> repairIngredient) {
-        return null;
-    }
+  public static Tier createTier(
+    int level,
+    int uses,
+    float speed,
+    float attackDamageBonus,
+    int enchantmentValue,
+    @NotNull TagKey<Block> tag,
+    @NotNull Supplier<Ingredient> repairIngredient
+  ) {
+    return null;
+  }
 }
--- fabric/src/main/java/dev/felnull/otyacraftengine/fabric/mixin/EntityMixin.java
+++ fabric/src/main/java/dev/felnull/otyacraftengine/fabric/mixin/EntityMixin.java
@@ -3,8 +3,6 @@ package dev.felnull.otyacraftengine.fabric.mixin;
 import dev.felnull.otyacraftengine.event.OECommonEventHooks;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,12 +12,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public class EntityMixin {
-    @Shadow
-    @Final
-    protected SynchedEntityData entityData;
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;defineSynchedData()V"))
-    private void init(EntityType<?> entityType, Level level, CallbackInfo ci) {
-        OECommonEventHooks.onEntityDefineSynchedData((Entity) (Object) this, entityData);
-    }
+  @Shadow
+  @Final
+  protected SynchedEntityData entityData;
+
+  @Inject(method = "<init>", at = @At("TAIL"))
+  private void init(CallbackInfo ci) {
+    OECommonEventHooks.onEntityDefineSynchedData(
+      (Entity) (Object) this,
+      entityData
+    );
+  }
 }
